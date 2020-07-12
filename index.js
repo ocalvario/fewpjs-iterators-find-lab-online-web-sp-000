221a65f64151a36328126fbc7e3e49928bbe523b
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-12 22:39:27 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let found = arr.find(rec => rec.result === "W")
+  return found ? found.year : undefined
+}
